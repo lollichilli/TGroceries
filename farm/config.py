74519b76a60a1eb5ecd4ed7d9eccee279daa8505
lollichilli <nguyth02@luther.edu>
2023-11-20 23:18:55 -0600

    Configured deployment

--- farm/config.py
+++ farm/config.py
@@ -9,7 +9,8 @@ from flask_marshmallow import Marshmallow
 app = Flask(__name__)
 
 this_dir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(this_dir, "products.sqlite3")
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(this_dir, "products.sqlite3")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://yklvxguuvotzto:3c1e9942c8036f27fd4eafc5d25b0dd40ab9e2201a4fdd7f3e8716f2f0f87dee@ec2-44-213-228-107.compute-1.amazonaws.com:5432/dfaoa2eatsqsnt"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 
